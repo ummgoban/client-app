d86edfd60d0e468d5b20ae12452be7784d2ca2a9
99mini <itman390@gmail.com>
2024-09-08 11:25:40 +0900

    chore: setting alias import

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,23 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@': './src',
+          '@components': './src/components',
+          '@context': './src/context',
+          '@navigation': './src/navigation',
+          '@screens': './src/screens',
+          '@hooks': './src/hooks',
+          '@utils': './src/utils',
+          // '@types': './src/types', // @types is typescript library, so use @/types instead of @types
+        },
+      },
+    ],
+  ],
   env: {
     production: {
       plugins: ['react-native-paper/babel'],

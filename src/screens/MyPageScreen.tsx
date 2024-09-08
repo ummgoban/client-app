3be27bf37c0363ba2a7e56711c6f089f3b606747
99mini <itman390@gmail.com>
2024-09-08 09:58:43 +0900

    fix: change foldername PascalCase, plural

--- src/screens/MyPageScreen.tsx
+++ src/screens/MyPageScreen.tsx
@@ -4,9 +4,9 @@ import React, {useCallback, useEffect} from 'react';
 import {Alert, View} from 'react-native';
 import {RefreshControl} from 'react-native-gesture-handler';
 import {Button} from 'react-native-paper';
-import {getOrderHistory} from '../api/Order';
-import {getUserProfile} from '../api/User';
-import {OrderHistory, Profile} from '../components/myPage';
+import {getOrderHistory} from '../apis/Order';
+import {getUserProfile} from '../apis/User';
+import {OrderHistory, Profile} from '../components/MyPage';
 import usePullDownRefresh from '../hooks/usePullDownRefresh';
 import {OrderType} from '../types/OrderType';
 import {

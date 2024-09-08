3701d141861972803d950fa13e95f93358bef4a9
99mini <itman390@gmail.com>
2024-09-08 09:49:46 +0900

    feat: split api folder

--- src/components/myPage/Profile.tsx
+++ src/components/myPage/Profile.tsx
@@ -3,10 +3,19 @@ import {Alert} from 'react-native';
 import {Text} from 'react-native-paper';
 import S from './Profile.style';
 
-const Profile = ({name, image}: {name: string; image: string}) => {
+const Profile = ({name, image}: {name: string; image?: string}) => {
   return (
     <S.ProfileContainer>
-      <S.ProfileImage source={{uri: image}} width={64} height={64} />
+      <S.ProfileImage
+        source={{
+          uri:
+            image ||
+            // TODO: 이미지 경로 수정
+            'https://media.istockphoto.com/id/1495088043/vector/user-profile-icon-avatar-or-person-icon-profile-picture-portrait-symbol-default-portrait.jpg?s=612x612&w=0&k=20&c=dhV2p1JwmloBTOaGAtaA3AW1KSnjsdMt7-U_3EZElZ0=',
+        }}
+        width={64}
+        height={64}
+      />
       <S.ProfileInfo>
         <Text>{`${name} 님`}</Text>
         <S.LinkLayout>
--- src/screens/MyPageScreen.tsx
+++ src/screens/MyPageScreen.tsx
@@ -1,9 +1,11 @@
 import styled from '@emotion/native';
 import {StackScreenProps} from '@react-navigation/stack';
-import React, {useEffect} from 'react';
+import React, {useCallback, useEffect} from 'react';
 import {Alert, View} from 'react-native';
 import {RefreshControl} from 'react-native-gesture-handler';
 import {Button} from 'react-native-paper';
+import {getOrderHistory} from '../api/Order';
+import {getUserProfile} from '../api/User';
 import {OrderHistory, Profile} from '../components/myPage';
 import usePullDownRefresh from '../hooks/usePullDownRefresh';
 import {OrderType} from '../types/OrderType';
@@ -11,6 +13,7 @@ import {
   HomeStackParamList,
   RootStackParamList,
 } from '../types/StackNavigationType';
+import {UserType} from '../types/UserType';
 
 type Props = StackScreenProps<
   HomeStackParamList & RootStackParamList,
@@ -19,119 +22,26 @@ type Props = StackScreenProps<
 
 const MyPageContainer = styled.ScrollView``;
 
-const dummyHistoryList: OrderType[] = [
-  {
-    id: 1,
-    store: {
-      id: 1,
-      name: 'very long store name very long store name',
-      image: 'https://legacy.reactjs.org/logo-og.png',
-    },
-    product: [
-      {
-        id: 1,
-        name: 'product1',
-        price: 1000,
-        count: 3,
-      },
-      {
-        id: 2,
-        name: 'product2',
-        price: 2000,
-        count: 1,
-      },
-    ],
-    pickupAt: 1719545600000,
-    createdAt: 1709545600000,
-    status: 'ORDERED',
-  },
-  {
-    id: 2,
-    store: {
-      id: 2,
-      name: 'store2',
-      image: 'https://legacy.reactjs.org/logo-og.png',
-    },
-    product: [
-      {
-        id: 2,
-        name: '띄어쓰기가없는엄청나게긴음식이름은어떻게해야될까요없는엄청나게긴음식이름은어떻게해야될까요',
-        price: 2000,
-        count: 1,
-      },
-    ],
-    pickupAt: 1614639000000,
-    createdAt: 1610639000000,
-    pendingAt: 1612639000000,
-    status: 'PENDING',
-  },
-  {
-    id: 3,
-    store: {
-      id: 3,
-      name: 'store3',
-      image: 'https://legacy.reactjs.org/logo-og.png',
-    },
-    product: [
-      {
-        id: 3,
-        name: 'very long product name + very long product name, very long product name, very long product name',
-        price: 3000,
-        count: 1,
-      },
-    ],
-    pickupAt: 1610718400000,
-    createdAt: 1609718400000,
-    pendingAt: 1609718400000,
-    doneAt: 1611718400000,
-    status: 'DONE',
-  },
-];
-
-// TODO: fetch order history
-const fetchOrderHistory = (): Promise<OrderType[] | null> => {
-  return new Promise(async resolve => {
-    await new Promise(_ => setTimeout(_, 1000));
-    console.log('fetch order history');
-    resolve(dummyHistoryList);
-  });
-};
-
-const UserMyPage = () => {
+const UserMyPage = ({profile}: {profile: UserType}) => {
   const [historyList, setHistoryList] = React.useState<OrderType[] | null>(
     null,
   );
 
-  const {refreshing, onRefresh} = usePullDownRefresh(async () => {
-    console.log('refreshing start');
-    const res = await fetchOrderHistory();
+  const fetchData = useCallback(async () => {
+    const res = await getOrderHistory();
     if (!res) {
       Alert.alert('주문 내역을 불러오는데 실패했습니다.');
       return;
     }
 
     setHistoryList(res);
-  });
+  }, []);
 
-  // TODO: fetch user profile
-  const profile = {
-    name: '김영민',
-    image: 'https://legacy.reactjs.org/logo-og.png',
-  };
+  const {refreshing, onRefresh} = usePullDownRefresh(fetchData);
 
   useEffect(() => {
-    const fetchData = async () => {
-      const res = await fetchOrderHistory();
-      if (!res) {
-        Alert.alert('주문 내역을 불러오는데 실패했습니다.');
-        return;
-      }
-
-      setHistoryList(res);
-    };
-
     fetchData();
-  }, []);
+  }, [fetchData]);
 
   return (
     <MyPageContainer
@@ -145,10 +55,23 @@ const UserMyPage = () => {
 };
 
 const MyPageScreen = ({navigation}: Props) => {
-  // TODO: fetch user session
-  const isLogin = true;
+  const [profile, setProfile] = React.useState<UserType | null>(null);
 
-  if (!isLogin) {
+  useEffect(() => {
+    const fetchProfile = async () => {
+      const res = await getUserProfile();
+      if (!res) {
+        Alert.alert('프로필을 불러오는데 실패했습니다.');
+        return;
+      }
+
+      setProfile(res);
+    };
+
+    fetchProfile();
+  }, []);
+
+  if (!profile) {
     return (
       <View>
         <Button
@@ -159,7 +82,7 @@ const MyPageScreen = ({navigation}: Props) => {
     );
   }
 
-  return <UserMyPage />;
+  return <UserMyPage profile={profile} />;
 };
 
 export default MyPageScreen;

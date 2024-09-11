42bf67176323e2bef74db37382e9d70f246576cd
itslitulinchpin2 <144733972+itslitulinchpin2@users.noreply.github.com>
2024-09-11 18:21:44 +0900

    feat: 피드페이지 개발 (#23)
    * feature: initial feed page
    
    * fix : 픽업시간 dayjs로 포맷팅 & 타입 수정
    
    * fix: SearchTab 수정
    
    * feat: useDebounce hook
    
    * chore: update eslint & prettier
    
    * fix: remove example code
    
    * fix: 코드리뷰 수정사항 반영
    
    * fix: Non-null 단언 연산자 수정
    
    * fix: Non-null 단언 연산자 추가수정
    
    * fix: store map key 수정
    
    * fix: 스토어 네이밍 수정
    
    * fix: navigator에서 store 네이밍 market으로 수정

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,2 +1,3 @@
 export * from './Order';
 export * from './User';
+export * from './Market';
---
+++ src/screens/FeedScreen/index.tsx
@@ -0,0 +1,63 @@
+import {getMarketList} from '@/apis';
+import {Market, SearchTab} from '@/components/feedPage';
+import usePullDownRefresh from '@/hooks/usePullDownRefresh';
+import {MarketType} from '@/types/Market';
+import {RootStackParamList} from '@/types/StackNavigationType';
+import {StackNavigationProp} from '@react-navigation/stack';
+import React, {useCallback, useEffect, useState} from 'react';
+import {Alert, RefreshControl, Text, View} from 'react-native';
+import S from './SearchBar.style';
+
+type Props = {
+  navigation: StackNavigationProp<RootStackParamList, 'Home'>;
+};
+
+const FeedScreen = ({navigation}: Props) => {
+  const [marketList, setMarketList] = useState<MarketType[] | null>(null);
+  const fetchData = useCallback(async () => {
+    const res = await getMarketList();
+    if (!res) {
+      Alert.alert('가게내역받아오기실패.');
+      return;
+    }
+    setMarketList(res);
+  }, []);
+
+  const onPressStore = (marketId: number) => {
+    navigation.navigate('Detail', {
+      screen: 'Market',
+      params: {marketId},
+    });
+  };
+
+  const {refreshing, onRefresh} = usePullDownRefresh(fetchData);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
+  if (!marketList) {
+    return (
+      <View>
+        <Text>가게목록을 불러오는데 실패했습니다.</Text>
+      </View>
+    );
+  }
+
+  return (
+    <S.Container>
+      <S.SearchWrapper>
+        <SearchTab />
+      </S.SearchWrapper>
+      <S.MarketWrapper
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }>
+        {marketList.map(market => (
+          <Market key={market.id} onPress={onPressStore} market={market} />
+        ))}
+      </S.MarketWrapper>
+    </S.Container>
+  );
+};
+export default FeedScreen;

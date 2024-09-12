f9b7d072d9b22f785f0387d7b024fe4609098dee
itslitulinchpin2 <itslitulinchpin@gmail.com>
2024-09-13 00:17:33 +0900

    feat: 장바구니 상태관리 기능 추가 (useState)

--- src/components/marketDetailPage/Menu.tsx
+++ src/components/marketDetailPage/Menu.tsx
@@ -1,7 +1,37 @@
 import {ProductType} from '@/types/ProductType';
 import React from 'react';
 import S from './Menu.style';
-const Menu = ({product}: {product: ProductType}) => {
+import {useState} from 'react';
+type Props = {
+  product: ProductType;
+  onCountChange: (
+    productId: number,
+    productName: string,
+    count: number,
+  ) => void;
+};
+const Menu = ({product, onCountChange}: Props) => {
+  const [menuCount, setMenuCount] = useState(0);
+
+  const menuCountButtonAdder = () => {
+    setMenuCount(prevCount => {
+      const newCount = prevCount + 1;
+      onCountChange(product.id, product.name, newCount);
+      return newCount;
+    });
+  };
+
+  const menuCountButtonReducer = () => {
+    setMenuCount(prevCount => {
+      const newCount = prevCount - 1;
+      if (newCount < 0) {
+        setMenuCount(0);
+        return 0;
+      }
+      onCountChange(product.id, product.name, newCount);
+      return newCount;
+    });
+  };
   return (
     <S.MenuWrapper>
       <S.MenuBoxLeft>
@@ -17,11 +47,11 @@ const Menu = ({product}: {product: ProductType}) => {
         <S.MenuImage source={{uri: product.image}} />
         <S.MenuTimeText>예약 마감 2시간 13분 전</S.MenuTimeText>
         <S.MenuCounter>
-          <S.MenuCounterButtonWrapper>
+          <S.MenuCounterButtonWrapper onPress={menuCountButtonReducer}>
             <S.MenuCounterButton>-</S.MenuCounterButton>
           </S.MenuCounterButtonWrapper>
-          <S.MenuCounterButton>0개</S.MenuCounterButton>
-          <S.MenuCounterButtonWrapper>
+          <S.MenuCounterButton>{menuCount} 개</S.MenuCounterButton>
+          <S.MenuCounterButtonWrapper onPress={menuCountButtonAdder}>
             <S.MenuCounterButton>+</S.MenuCounterButton>
           </S.MenuCounterButtonWrapper>
         </S.MenuCounter>
--- src/screens/MarketDetailScreen/MarketDetail.style.tsx
+++ src/screens/MarketDetailScreen/MarketDetail.style.tsx
@@ -4,7 +4,55 @@ const MarketDetailInfoView = styled.View`
   flex: 1;
   padding: 16px;
 `;
+const MarketMainInfoWrapper = styled.View`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 8px;
+  align-self: stretch;
+`;
+const MarKetName = styled.Text`
+  color: #2d3e39;
+  text-align: center;
+  font-size: 24px;
+  font-weight: 700;
+  line-height: 28.5px;
+`;
+const MarketDescription = styled.Text`
+  color: #2d3e39;
+  font-family: Pretendard;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 120%;
+  letter-spacing: -0.12px;
+`;
+
+const MarketSideInfoWrapper = styled.View`
+  display: flex;
+  padding: 16px 0px;
+  flex-direction: column;
+  align-items: flex-start;
+  align-self: stretch;
+`;
+
+const MarketSideInfo = styled.Text`
+  color: #78827f;
+
+  font-family: Pretendard;
+  font-size: 13px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+`;
+
+const Divider = styled.View`
+  width: 390px;
+  height: 8px;
+  background: #ecf3f1;
+`;
 const ReserveButton = styled.TouchableOpacity`
+  margin-top: 15px;
   display: flex;
   align-items: center;
   background-color: lightblue;
@@ -21,6 +69,12 @@ const ButtonText = styled.Text`
 
 const S = {
   MarketDetailInfoView,
+  MarketMainInfoWrapper,
+  MarKetName,
+  MarketDescription,
+  MarketSideInfo,
+  MarketSideInfoWrapper,
+  Divider,
   ReserveButton,
   ButtonText,
 };

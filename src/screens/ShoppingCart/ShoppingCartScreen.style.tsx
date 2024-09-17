1b8f112218a5c2653edcc9823fd2d7b0f9c9a505
l-lyun <kimdohyun032@gmail.com>
2024-09-17 22:24:53 +0900

    feat: cart 타입 작성

---
+++ src/screens/ShoppingCart/ShoppingCartScreen.style.tsx
@@ -0,0 +1,12 @@
+import styled from "@emotion/native";
+
+const CartPage = styled.View`
+  flex: 1;
+  margin: 0 16px;
+`
+
+const ScrollView = styled.ScrollView``;
+
+const S = {CartPage, ScrollView}
+
+export default S;

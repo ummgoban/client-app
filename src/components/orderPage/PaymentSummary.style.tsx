a6c7ca1e49729b8b929da4c3bca32f4dceb68aef
YoungMin <43674669+99mini@users.noreply.github.com>
2024-09-19 01:47:01 +0900

    feat: 결제하기 페이지 작성합니다. (#28)
    * feat: 결제 페이지 레이아웃
    
    * feat: use react-native-paper
    
    * fix: card style
    
    * fix: refactoring common component
    
    * feat: 시간 픽커 작성
    
    * fix: export using index
    
    * fix: refactoring Common component

--- src/components/orderPage/PaymentSummary.style.tsx
+++ src/components/orderPage/PaymentSummary.style.tsx
@@ -1,27 +1,18 @@
 import styled from '@emotion/native';
+import C from './Common.style';
 
-const PaymentSummaryContainer = styled.View`
-  padding: 16px;
-
-  display: flex;
-  flex-direction: column;
-
-  border: 1px solid #b5b5b5;
-`;
+const Card = styled(C.CommonCard)``;
+const HeaderText = styled(C.HeaderText)``;
 
 const Header = styled.View`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
+
   margin-bottom: 16px;
-`;
 
-const HeaderText = styled.Text`
-  font-size: 20px;
-  line-height: 34px;
-  font-weight: 600;
-
-  color: black;
+  border-bottom-width: 1px;
+  border-bottom-color: #b5b5b5;
 `;
 
 const PaymentSummaryItemList = styled.View`
@@ -45,11 +36,11 @@ const ItemText = styled.Text<{primary?: boolean}>`
 `;
 
 const S = {
-  PaymentSummaryContainer,
+  Card,
+  HeaderText,
   PaymentSummaryItemContainer,
   PaymentSummaryItemList,
   Header,
-  HeaderText,
   ItemText,
 };
 

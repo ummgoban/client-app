347404a86d9f6e6e611f0147c087722aa3649fe8
l-lyun <kimdohyun032@gmail.com>
2024-09-27 15:08:45 +0900

    feat: 반찬 컴포넌트 통일 및 타입명 수정

--- src/components/CartPage/MarketInfo.tsx
+++ src/components/CartPage/MarketInfo.tsx
@@ -3,7 +3,7 @@ import S from './MarketInfo.style';
 import {MarketType} from '@/types/Market';
 
 type MarketInfoProps = {
-  market: MarketType;
+  market: Pick<MarketType, 'id' | 'name' | 'images'>;
   onPress: () => void;
 };
 const MarketInfo = ({market, onPress}: MarketInfoProps) => {

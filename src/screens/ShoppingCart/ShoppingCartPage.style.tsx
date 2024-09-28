8410c5a40544f0675af755e6e42d70a324f78a1b
l-lyun <kimdohyun032@gmail.com>
2024-09-28 11:27:22 +0900

    반찬 컴포넌트 삭제 버튼 추가

--- src/screens/ShoppingCart/ShoppingCartPage.style.tsx
+++ src/screens/ShoppingCart/ShoppingCartPage.style.tsx
@@ -16,88 +16,10 @@ const CardContainer = styled.View`
   background-color: white;
 `;
 
-const ProductImageWrapper = styled.View`
-  flex: 0.3;
-  justify-content: center;
-  align-items: center;
-`;
-
-const ProductInfoWrapper = styled.View`
-  flex: 0.7;
-  padding-left: 12px;
-`;
-
-const ProductImage = styled.Image`
-  width: 70px;
-  height: 70px;
-  border-radius: 8px;
-`;
-
-const HeaderText = styled(Common.HeaderText)`
-  margin-bottom: 4px;
-`;
-
-const DetailText = styled(Common.BodyText)`
-  color: #757575;
-`;
-
-const PriceText = styled.Text`
-  font-size: 18px;
-  font-weight: 600;
-  margin-top: 8px;
-`;
-
-const ButtonWrapper = styled.View`
-  flex-direction: row;
-  align-items: center;
-  margin-top: 12px;
-`;
-
-const Button = styled.TouchableOpacity`
-  background-color: #f0f0f0;
-  padding: 8px;
-  border-radius: 4px;
-  margin: 0 8px;
-`;
-
-const CountText = styled.Text`
-  font-size: 16px;
-  font-weight: 500;
-`;
-
-const PaymentButtonContainer = styled.View({
-  padding: 16,
-  backgroundColor: '#fff',
-});
-
-const PaymentButton = styled(RNPButton)`
-  width: 100%;
-  border-radius: 8px;
-  margin-bottom: 16px;
-`;
-
-const PaymentButtonText = styled.Text({
-  color: '#fff',
-  fontSize: 18,
-  fontWeight: 'bold',
-});
-
 const S = {
   CartPage,
   CardContainer,
-  ProductImageWrapper,
-  ProductInfoWrapper,
-  ProductImage,
-  HeaderText,
-  DetailText,
-  PriceText,
-  ButtonWrapper,
-  Button,
-  CountText,
   ScrollView,
-  PaymentButtonContainer,
-  PaymentButton,
-  PaymentButtonText,
 };
 
 export default S;

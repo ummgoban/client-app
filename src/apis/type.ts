68fcddb01fae10ed6f9bda1b7bae2883bc9d51dd
l-lyun <kimdohyun032@gmail.com>
2024-10-10 02:46:31 +0900

    fix: 미사용 타입 제거

--- src/apis/type.ts
+++
@@ -1,8 +0,0 @@
-import {AxiosResponse} from 'axios';
-export type ServiceFunc<Params = unknown | undefined, Res = unknown> = (
-  params: Params,
-) => Promise<AxiosResponse<Res>>;
-
-export type ServiceFuncWithoutParams<Res = unknown> = () => Promise<
-  AxiosResponse<Res>
->;

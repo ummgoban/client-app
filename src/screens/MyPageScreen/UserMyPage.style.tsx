73fa02dce2459d3b959b71cc42bf1e0ef276fd15
l-lyun <kimdohyun032@gmail.com>
2024-10-29 01:11:11 +0900

    feat: 프로필 refresh 추가

--- src/screens/MyPageScreen/UserMyPage.style.tsx
+++ src/screens/MyPageScreen/UserMyPage.style.tsx
@@ -1,9 +1,50 @@
 import styled from '@emotion/native';
 
-const MyPageContainer = styled.ScrollView``;
+const MyPageContainer = styled.ScrollView`
+  margin: 32px 16px;
+`;
 
+const NoticeSection = styled.View`
+  display: flex;
+  margin: 48px 16px;
+  width: 75%;
+  gap: 8px;
+`;
+
+const NoticeSectionTitle = styled.Text`
+  font-size: 16px;
+  margin-bottom: 8px;
+  color: #888;
+`;
+
+const ButtonRow = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  margin-bottom: 16px;
+`;
+
+const VerticalDivider = styled.View`
+  border: 1px solid #888;
+`;
+
+const BottomSectionContainer = styled.View`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+`;
+
+const BottomSection = styled.View`
+  width: 40%;
+`;
 const S = {
   MyPageContainer,
+  NoticeSectionTitle,
+  ButtonRow,
+  NoticeSection,
+  VerticalDivider,
+  BottomSectionContainer,
+  BottomSection,
 };
 
 export default S;
--- src/screens/MyPageScreen/UserMyPage.tsx
+++ src/screens/MyPageScreen/UserMyPage.tsx
@@ -1,45 +1,96 @@
-import React, {useCallback, useEffect, useState} from 'react';
+import React from 'react';
 import {Alert, RefreshControl} from 'react-native';
-import {OrderType} from '@/types/OrderType';
-import {RootStackParamList} from '@/types/StackNavigationType';
 import {UserType} from '@/types/UserType';
-import {getOrderHistory} from '@apis/Order';
 import {Profile} from '@components/myPage';
-import usePullDownRefresh from '@hooks/usePullDownRefresh';
+import {logout} from '@/apis/Login';
 import {useNavigation} from '@react-navigation/native';
 import {StackNavigationProp} from '@react-navigation/stack';
-
+import ListBox from '@/components/common/ListBox';
+import NavigateTextButton from '@/components/common/NavigateTextButton';
 import S from './UserMyPage.style';
+import {RootStackParamList} from '@/types/StackNavigationType';
 
-const UserMyPage = ({profile}: {profile: UserType}) => {
-  const [historyList, setHistoryList] = useState<OrderType[] | null>(null);
+type UserMyPageProps = {
+  profile: UserType;
+  refreshing: boolean;
+  onRefresh: () => void;
+};
 
+const UserMyPage = ({profile, refreshing, onRefresh}: UserMyPageProps) => {
   const navigation =
     useNavigation<StackNavigationProp<RootStackParamList, 'Detail'>>();
 
-  const fetchData = useCallback(async () => {
-    const res = await getOrderHistory();
-    if (!res) {
-      Alert.alert('주문 내역을 불러오는데 실패했습니다.');
-      return;
-    }
-
-    setHistoryList(res);
-  }, []);
-
-  const {refreshing, onRefresh} = usePullDownRefresh(fetchData);
-
-  useEffect(() => {
-    fetchData();
-  }, [fetchData]);
-
   return (
     <S.MyPageContainer
-      // TODO: 영민이형 컨펌 이후 리프레쉬 로직 지우기
       refreshControl={
         <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
       }>
-      <Profile name={profile.name} image={profile.image} />
+      <Profile image={profile.image} />
+      <ListBox
+        items={[
+          {label: '이름', value: `${profile.name}`},
+          {label: '이메일', value: `test@example.com`},
+          {label: '전화번호', value: `010-1234-5678`},
+        ]}
+      />
+      <S.NoticeSection>
+        <S.NoticeSectionTitle>문의 및 알림</S.NoticeSectionTitle>
+        <S.ButtonRow>
+          <NavigateTextButton
+            text="공지사항"
+            fontSize="20px"
+            onPress={() => Alert.alert('공지사항 화면으로 이동')}
+          />
+          <NavigateTextButton
+            text="약관 및 정책"
+            fontSize="20px"
+            onPress={() => Alert.alert('약관 및 정책 화면으로 이동')}
+          />
+        </S.ButtonRow>
+        <S.ButtonRow>
+          <NavigateTextButton
+            text="공지사항"
+            fontSize="20px"
+            onPress={() => Alert.alert('공지사항 화면으로 이동')}
+          />
+          <NavigateTextButton
+            text="약관 및 정책"
+            fontSize="20px"
+            onPress={() => Alert.alert('약관 및 정책 화면으로 이동')}
+          />
+        </S.ButtonRow>
+      </S.NoticeSection>
+      <S.BottomSectionContainer>
+        <S.BottomSection>
+          <S.ButtonRow>
+            <NavigateTextButton
+              text="로그아웃"
+              fontSize="12px"
+              fontColor="#888"
+              onPress={async () => {
+                const res = await logout();
+                if (res) {
+                  Alert.alert('로그아웃 되었습니다.', '', [
+                    {
+                      text: '확인',
+                      onPress: () => {
+                        navigation.navigate('Home', {screen: 'Feed'});
+                      },
+                    },
+                  ]);
+                }
+              }}
+            />
+            <S.VerticalDivider />
+            <NavigateTextButton
+              text="회원탈퇴"
+              fontSize="12px"
+              fontColor="#888"
+              onPress={() => Alert.alert('회원탈퇴')}
+            />
+          </S.ButtonRow>
+        </S.BottomSection>
+      </S.BottomSectionContainer>
     </S.MyPageContainer>
   );
 };

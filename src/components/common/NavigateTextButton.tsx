b14cb74341ccc891302c0b0b6a670837d6670636
l-lyun <142032659+l-lyun@users.noreply.github.com>
2024-10-29 16:15:56 +0900

    feat: 마이페이지 제작 및 주문 내역 분리 (#59)
    * feat: 마이페이지 주문내역 분리
    
    * feat: NavigateTextButton 공통 컴포넌트 제작
    
    * feat: 프로필 refresh 추가
    
    * feat: Mypage 페이지 제작 및 디자인 적용
    
    * fix: 주석 삭제
    
    * fix: 컴포넌트 스타일 변경
    
    * fix: tsc error
    
    * feat: NaviagtionTextButton 컴포넌트 통합 및 ListBox 옵셔널 입력 추가
    
    * fix: Redundant Boolean call

---
+++ src/components/common/NavigateTextButton.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import S from './NavigateTextButton.style';
+import Icon from 'react-native-vector-icons/AntDesign';
+
+type NavigationTextButtonProps = {
+  text: string;
+  onPress?: () => void;
+  fontColor?: string;
+  fontSize?: string;
+  iconSize?: number;
+  isNotice?: boolean;
+};
+
+const NavigationTextButton = ({
+  text,
+  onPress,
+  fontColor = '#000',
+  fontSize = '16px',
+  iconSize = 16,
+  isNotice = true,
+}: NavigationTextButtonProps) => {
+  return (
+    <S.TouchableButtonContainer onPress={onPress} disabled={!onPress}>
+      <S.TouchableWrapper>
+        <S.NoticeText fontColor={fontColor} fontSize={fontSize}>
+          {text}
+        </S.NoticeText>
+        {isNotice && <Icon name="right" size={iconSize} color={fontColor} />}
+      </S.TouchableWrapper>
+    </S.TouchableButtonContainer>
+  );
+};
+
+export default NavigationTextButton;
--- src/components/myPage/Profile.tsx
+++ src/components/myPage/Profile.tsx
@@ -1,17 +1,8 @@
-import {useNavigation} from '@react-navigation/native';
-import {StackNavigationProp} from '@react-navigation/stack';
 import React from 'react';
-import {Alert} from 'react-native';
-import {Button, Text} from 'react-native-paper';
-
-import {logout} from '@/apis/Login';
-import {RootStackParamList} from '@/types/StackNavigationType';
 
 import S from './Profile.style';
 
-const Profile = ({name, image}: {name: string; image?: string}) => {
-  const navigation = useNavigation<StackNavigationProp<RootStackParamList>>();
-
+const Profile = ({image}: {image?: string}) => {
   return (
     <S.ProfileContainer>
       <S.ProfileImage
@@ -21,40 +12,7 @@ const Profile = ({name, image}: {name: string; image?: string}) => {
             // TODO: 이미지 경로 수정
             'https://media.istockphoto.com/id/1495088043/vector/user-profile-icon-avatar-or-person-icon-profile-picture-portrait-symbol-default-portrait.jpg?s=612x612&w=0&k=20&c=dhV2p1JwmloBTOaGAtaA3AW1KSnjsdMt7-U_3EZElZ0=',
         }}
-        width={64}
-        height={64}
       />
-      <S.ProfileInfo>
-        <Text>{`${name} 님`}</Text>
-        <S.LinkLayout>
-          <S.LinkContainer>
-            <S.Link onPress={() => Alert.alert('주문 내역으로 이동')}>
-              <S.LinkText>주문 내역</S.LinkText>
-            </S.Link>
-          </S.LinkContainer>
-          <S.LinkContainer>
-            <S.Link onPress={() => Alert.alert('프로필로 이동')}>
-              <S.LinkText>프로필</S.LinkText>
-            </S.Link>
-          </S.LinkContainer>
-          <Button
-            onPress={async () => {
-              const res = await logout();
-              if (res) {
-                Alert.alert('로그아웃 되었습니다.', '', [
-                  {
-                    text: '확인',
-                    onPress: () => {
-                      navigation.navigate('Home', {screen: 'Feed'});
-                    },
-                  },
-                ]);
-              }
-            }}>
-            로그아웃
-          </Button>
-        </S.LinkLayout>
-      </S.ProfileInfo>
     </S.ProfileContainer>
   );
 };

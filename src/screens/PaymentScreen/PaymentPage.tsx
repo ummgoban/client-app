e996f8b8392951f4d647e1cf7cfd3b1e33dcee0e
99mini <itman390@gmail.com>
2024-10-30 17:48:28 +0900

    fix: remove unuse code

--- src/screens/PaymentScreen/PaymentPage.tsx
+++ src/screens/PaymentScreen/PaymentPage.tsx
@@ -1,7 +1,15 @@
 import React, {useMemo, useState} from 'react';
+import {Alert} from 'react-native';
 
 import {useNavigation} from '@react-navigation/native';
 import {StackNavigationProp} from '@react-navigation/stack';
+
+import {
+  AgreementWidget,
+  AgreementWidgetControl,
+  PaymentMethodWidget,
+  usePaymentWidget,
+} from '@tosspayments/widget-sdk-react-native';
 
 import {CartType} from '@/types/OrderType';
 import {RootStackParamList} from '@/types/StackNavigationType';
@@ -9,15 +17,6 @@ import {BottomButton} from '@components/common';
 import {DatePickerCard, PaymentSummary} from '@components/orderPage';
 
 import S from './PaymentPage.style';
-
-import {
-  AgreementWidget,
-  AgreementWidgetControl,
-  PaymentMethodWidget,
-  PaymentMethodWidgetControl,
-  usePaymentWidget,
-} from '@tosspayments/widget-sdk-react-native';
-import {Button, Alert} from 'react-native';
 
 type Props = {cart: CartType};
 
@@ -37,8 +36,6 @@ const PaymentPage = ({cart}: Props) => {
   );
 
   const paymentWidgetControl = usePaymentWidget();
-  const [paymentMethodWidgetControl, setPaymentMethodWidgetControl] =
-    useState<PaymentMethodWidgetControl | null>(null);
   const [agreementWidgetControl, setAgreementWidgetControl] =
     useState<AgreementWidgetControl | null>(null);
 
@@ -59,7 +56,7 @@ const PaymentPage = ({cart}: Props) => {
                   },
                 )
                 .then(control => {
-                  setPaymentMethodWidgetControl(control);
+                  console.log({control});
                 });
             }}
           />
@@ -75,65 +72,6 @@ const PaymentPage = ({cart}: Props) => {
                 });
             }}
           />
-          <Button
-            title="결제요청"
-            onPress={async () => {
-              if (
-                paymentWidgetControl == null ||
-                agreementWidgetControl == null
-              ) {
-                Alert.alert('주문 정보가 초기화되지 않았습니다.');
-                return;
-              }
-
-              const agreeement =
-                await agreementWidgetControl.getAgreementStatus();
-              if (agreeement.agreedRequiredTerms !== true) {
-                Alert.alert('약관에 동의하지 않았습니다.');
-                return;
-              }
-
-              paymentWidgetControl
-                .requestPayment?.({
-                  orderId: '1lB4sMvBNySuMmzDy5PPv',
-                  orderName: '토스 티셔츠 외 2건',
-                })
-                .then(result => {
-                  if (result?.success) {
-                    // 결제 성공 비즈니스 로직을 구현하세요.
-                    // result.success에 있는 값을 서버로 전달해서 결제 승인을 호출하세요.
-                  } else if (result?.fail) {
-                    // 결제 실패 비즈니스 로직을 구현하세요.
-                  }
-                });
-            }}
-          />
-          <Button
-            title="선택된 결제수단"
-            onPress={async () => {
-              if (paymentMethodWidgetControl == null) {
-                Alert.alert('주문 정보가 초기화되지 않았습니다.');
-                return;
-              }
-
-              Alert.alert(
-                `선택된 결제수단: ${JSON.stringify(await paymentMethodWidgetControl.getSelectedPaymentMethod())}`,
-              );
-            }}
-          />
-          <Button
-            title="결제 금액 변경"
-            onPress={() => {
-              if (paymentMethodWidgetControl == null) {
-                Alert.alert('주문 정보가 초기화되지 않았습니다.');
-                return;
-              }
-
-              paymentMethodWidgetControl.updateAmount(100_000).then(() => {
-                Alert.alert('결제 금액이 100000원으로 변경되었습니다.');
-              });
-            }}
-          />
         </>
         <PaymentSummary
           originalPrice={originalPrice}
@@ -141,12 +79,37 @@ const PaymentPage = ({cart}: Props) => {
         />
       </S.ScrollView>
       <BottomButton
-        onPress={() =>
-          navigation.navigate('Detail', {
-            screen: 'OrderDone',
-            params: {orderId: 1},
-          })
-        }>
+        onPress={async () => {
+          if (paymentWidgetControl == null || agreementWidgetControl == null) {
+            Alert.alert('주문 정보가 초기화되지 않았습니다.');
+            return;
+          }
+
+          const agreeement = await agreementWidgetControl.getAgreementStatus();
+          if (agreeement.agreedRequiredTerms !== true) {
+            Alert.alert('약관에 동의하지 않았습니다.');
+            return;
+          }
+
+          paymentWidgetControl
+            .requestPayment?.({
+              orderId: '1lB4sMvBNySuMmzDy5PPv',
+              orderName: '토스 티셔츠 외 2건',
+            })
+            .then(result => {
+              if (result?.success) {
+                // 결제 성공 비즈니스 로직을 구현하세요.
+                // result.success에 있는 값을 서버로 전달해서 결제 승인을 호출하세요.
+                navigation.navigate('Detail', {
+                  screen: 'OrderDone',
+                  params: {orderId: 1},
+                });
+              } else if (result?.fail) {
+                // 결제 실패 비즈니스 로직을 구현하세요.
+                Alert.alert('결제 실패');
+              }
+            });
+        }}>
         {`${discountPrice.toLocaleString()}원 결제하기`}
       </BottomButton>
     </S.PaymentPage>

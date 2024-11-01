92ba517bc4fb60091d19248f0493c6124ba4ffd8
99mini <43674669+99mini@users.noreply.github.com>
2024-11-01 15:50:36 +0900

    chore: 토스 페이먼츠 세팅 (#67)
    * chore: 토스 페이먼츠 세팅
    
    * chore: pod install
    
    * fix: remove unuse code

--- src/screens/PaymentScreen/PaymentPage.tsx
+++ src/screens/PaymentScreen/PaymentPage.tsx
@@ -1,8 +1,15 @@
-import React, {useMemo} from 'react';
-import {Text} from 'react-native-paper';
+import React, {useMemo, useState} from 'react';
+import {Alert} from 'react-native';
 
 import {useNavigation} from '@react-navigation/native';
 import {StackNavigationProp} from '@react-navigation/stack';
+
+import {
+  AgreementWidget,
+  AgreementWidgetControl,
+  PaymentMethodWidget,
+  usePaymentWidget,
+} from '@tosspayments/widget-sdk-react-native';
 
 import {CartType} from '@/types/OrderType';
 import {RootStackParamList} from '@/types/StackNavigationType';
@@ -28,23 +35,81 @@ const PaymentPage = ({cart}: Props) => {
     [cart.products],
   );
 
+  const paymentWidgetControl = usePaymentWidget();
+  const [agreementWidgetControl, setAgreementWidgetControl] =
+    useState<AgreementWidgetControl | null>(null);
+
   return (
     <S.PaymentPage>
       <S.ScrollView>
         <DatePickerCard />
-        <Text>{'// TODO: 결제수단 서버에서 받아와야 함'}</Text>
+        <>
+          <PaymentMethodWidget
+            selector="payment-methods"
+            onLoadEnd={() => {
+              paymentWidgetControl
+                .renderPaymentMethods(
+                  'payment-methods',
+                  {value: 50000},
+                  {
+                    variantKey: 'DEFAULT',
+                  },
+                )
+                .then(control => {
+                  console.log({control});
+                });
+            }}
+          />
+          <AgreementWidget
+            selector="agreement"
+            onLoadEnd={() => {
+              paymentWidgetControl
+                .renderAgreement('agreement', {
+                  variantKey: 'DEFAULT',
+                })
+                .then(control => {
+                  setAgreementWidgetControl(control);
+                });
+            }}
+          />
+        </>
         <PaymentSummary
           originalPrice={originalPrice}
           discountPrice={discountPrice}
         />
       </S.ScrollView>
       <BottomButton
-        onPress={() =>
-          navigation.navigate('Detail', {
-            screen: 'OrderDone',
-            params: {orderId: 1},
-          })
-        }>
+        onPress={async () => {
+          if (paymentWidgetControl == null || agreementWidgetControl == null) {
+            Alert.alert('주문 정보가 초기화되지 않았습니다.');
+            return;
+          }
+
+          const agreeement = await agreementWidgetControl.getAgreementStatus();
+          if (agreeement.agreedRequiredTerms !== true) {
+            Alert.alert('약관에 동의하지 않았습니다.');
+            return;
+          }
+
+          paymentWidgetControl
+            .requestPayment?.({
+              orderId: '1lB4sMvBNySuMmzDy5PPv',
+              orderName: '토스 티셔츠 외 2건',
+            })
+            .then(result => {
+              if (result?.success) {
+                // 결제 성공 비즈니스 로직을 구현하세요.
+                // result.success에 있는 값을 서버로 전달해서 결제 승인을 호출하세요.
+                navigation.navigate('Detail', {
+                  screen: 'OrderDone',
+                  params: {orderId: 1},
+                });
+              } else if (result?.fail) {
+                // 결제 실패 비즈니스 로직을 구현하세요.
+                Alert.alert('결제 실패');
+              }
+            });
+        }}>
         {`${discountPrice.toLocaleString()}원 결제하기`}
       </BottomButton>
     </S.PaymentPage>

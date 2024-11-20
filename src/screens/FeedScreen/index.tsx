edb4e2e53971c1d2b915511772f995ab81bd6b97
99mini <itman390@gmail.com>
2024-11-20 10:50:08 +0900

    feat: add filtering logic for market list based on available products

--- src/screens/FeedScreen/index.tsx
+++ src/screens/FeedScreen/index.tsx
@@ -23,6 +23,7 @@ type Props = {
 
 const FeedScreen = ({navigation}: Props) => {
   const [marketList, setMarketList] = useState<MarketType[] | null>(null);
+
   const [location, setLocation] = useState<{
     userLatitude: number;
     userLongitude: number;
@@ -39,7 +40,8 @@ const FeedScreen = ({navigation}: Props) => {
       Alert.alert('가게내역받아오기실패.');
       return;
     }
-    setMarketList(res.markets);
+    // TODO: 필터링 로직 추가
+    setMarketList(res.markets.filter(market => market.products.length));
   }, [location]);
 
   // const getCurrentLocation = useCallback(() => {

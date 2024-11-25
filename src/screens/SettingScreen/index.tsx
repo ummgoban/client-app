69ccc5a834fac712f6a02a1791560ff07ce04347
99mini <itman390@gmail.com>
2024-11-25 20:32:58 +0900

    feat: implement SettingScreen with notification and location toggle switches

--- src/screens/SettingScreen/index.tsx
+++ src/screens/SettingScreen/index.tsx
@@ -1,11 +1,56 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState} from 'react';
+
+import S from './SettingScreen.style';
 
 const SettingScreen = () => {
+  const [isNotificationOn, setIsNotificationOn] = useState(false);
+  const [isLocationOn, setIsLocationOn] = useState(false);
+
+  // TODO: Implement handleNotificationSwitch function
+  const handleNotificationSwitch = async () => {
+    setIsNotificationOn(prev => !prev);
+  };
+
+  // TODO: Implement handleLocationSwitch function
+  const handleLocationSwitch = async () => {
+    setIsLocationOn(prev => !prev);
+  };
+
   return (
-    <View>
-      <Text>SettingScreen</Text>
-    </View>
+    <S.Container>
+      <S.SettingItemList>
+        <S.SettingItemTitle>알림</S.SettingItemTitle>
+        <S.SettingItem>
+          <S.SettingItemDescriptionContainer>
+            <S.SettingItemDescriptionTitle>
+              찜한 가게 알림
+            </S.SettingItemDescriptionTitle>
+            <S.SettingItemDescription>
+              찜한 가게에 새로운 상품이 등록되면 알림을 보내드려요
+            </S.SettingItemDescription>
+          </S.SettingItemDescriptionContainer>
+          <S.SwitchButton
+            value={isNotificationOn}
+            onChange={handleNotificationSwitch}
+          />
+        </S.SettingItem>
+        <S.SettingItemTitle>위치</S.SettingItemTitle>
+        <S.SettingItem>
+          <S.SettingItemDescriptionContainer>
+            <S.SettingItemDescriptionTitle>
+              현재 위치
+            </S.SettingItemDescriptionTitle>
+            <S.SettingItemDescription>
+              현재 위치를 사용하여 가까운 가게를 추천해드려요
+            </S.SettingItemDescription>
+          </S.SettingItemDescriptionContainer>
+          <S.SwitchButton
+            value={isLocationOn}
+            onChange={handleLocationSwitch}
+          />
+        </S.SettingItem>
+      </S.SettingItemList>
+    </S.Container>
   );
 };
 

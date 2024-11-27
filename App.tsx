219837772a65ae9f34f7f8e707575c66c58dff40
l-lyun <kimdohyun032@gmail.com>
2024-11-27 11:34:39 +0900

    feat: splash-screen 적용

--- App.tsx
+++ App.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './gesture-handler';
 import RootProvider from './src/context';
 import AppNavigator from './src/navigation';
-
+import SplashScreen from 'react-native-splash-screen';
+import {requestUserPermission} from '@/utils/notification';
 function App(): React.JSX.Element {
   // const isDarkMode = useColorScheme() === 'dark';
-
+  useEffect(() => {
+    const initializeApp = async () => {
+      try {
+        // TODO: 권한 요청 로직 호출
+      } catch (error) {
+        console.error('splash screen error:', error);
+      } finally {
+        SplashScreen.hide();
+        console.log('splash screen hide');
+      }
+    };
+    initializeApp();
+  }, []);
   return (
     <RootProvider>
       <AppNavigator />

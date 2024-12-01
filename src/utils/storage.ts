8668e01fdbfd3ae7d5210d504fd4befae9e45d9c
l-lyun <142032659+l-lyun@users.noreply.github.com>
2024-12-02 00:57:59 +0900

    feat: 세팅 페이지 알림 권한 (#101)
    * fix: notifictaion 함수 리팩토링
    
    * feat: notification toggle
    
    * feat: 셋팅페이지 스플래시스크린 알림 권한 기능 분리
    
    * fix: fcm 권한 로직 수정
    
    * feat: 초기 유저 대응 asnycStorage 추가 및 알림 로직 수정
    
    * fix: EncryptedStorage로 변경

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,6 +1,7 @@
 import EncryptedStorage from 'react-native-encrypted-storage';
-
 import {StorageKeyType} from '@/types/Storage';
+
+const FIRST_LAUNCH_KEY = 'isFirst';
 
 export const setStorage = async <T extends Object>(
   key: StorageKeyType,
@@ -14,4 +15,18 @@ export const getStorage = async <T extends Object>(
 ): Promise<T | null> => {
   const value = await EncryptedStorage.getItem(key);
   return value ? (JSON.parse(value) as T) : null;
+};
+
+export const checkFirstLaunch = async (): Promise<boolean> => {
+  try {
+    const isFirstLaunch = await EncryptedStorage.getItem(FIRST_LAUNCH_KEY);
+    if (isFirstLaunch === null) {
+      await EncryptedStorage.setItem(FIRST_LAUNCH_KEY, JSON.stringify(false));
+      return true;
+    }
+    return false;
+  } catch (error) {
+    console.error('Error checking first launch:', error);
+    return false;
+  }
 };

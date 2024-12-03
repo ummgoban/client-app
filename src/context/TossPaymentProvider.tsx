afb4c52013fb4928116cf865cea21d3a2d154a4a
99mini <itman390@gmail.com>
2024-12-03 17:09:25 +0900

    fix: rn config 적용

--- src/context/TossPaymentProvider.tsx
+++ src/context/TossPaymentProvider.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import {PaymentWidgetProvider} from '@tosspayments/widget-sdk-react-native';
+import Config from 'react-native-config';
 
 // TODO: Replace the clientKey and customerKey with your own keys
 const TossPaymentProvider = ({children}: {children: React.ReactNode}) => {
   return (
     <PaymentWidgetProvider
-      clientKey={process.env.TOSS_CLIENT_KEY as string}
-      customerKey={`wGidPqu8iPkhWTQaqm_Ad`}>
+      clientKey={Config.TOSS_CLIENT_KEY}
+      customerKey={Config.TOSS_CUSTOMER_KEY}>
       {children}
     </PaymentWidgetProvider>
   );

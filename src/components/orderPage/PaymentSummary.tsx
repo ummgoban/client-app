dd49e30c699b914c184a7888d42199886e83804e
l-lyun <kimdohyun032@gmail.com>
2024-12-11 19:01:53 +0900

    fix: 예약하기 플로우에 맞춘 워딩 변경

--- src/components/orderPage/PaymentSummary.tsx
+++ src/components/orderPage/PaymentSummary.tsx
@@ -23,6 +23,7 @@ type Props = {
   discountPrice: number;
 };
 
+// TODO: HeaderPaymentMethod 삭제 후 border-bottom 이동
 const PaymentSummary = ({originPrice, discountPrice}: Props) => {
   return (
     <S.Card>
@@ -30,6 +31,10 @@ const PaymentSummary = ({originPrice, discountPrice}: Props) => {
         <S.HeaderText>결제금액</S.HeaderText>
         <S.HeaderText>{`${discountPrice.toLocaleString()}원`}</S.HeaderText>
       </S.Header>
+      <S.HeaderPaymentMethod>
+        <S.HeaderText>결제방법 </S.HeaderText>
+        <S.HeaderText>만나서 결제</S.HeaderText>
+      </S.HeaderPaymentMethod>
       <S.PaymentSummaryItemList>
         <PaymentSummaryItem
           title="주문금액"
@@ -38,6 +43,11 @@ const PaymentSummary = ({originPrice, discountPrice}: Props) => {
         <PaymentSummaryItem
           title="할인금액"
           price={`-${(originPrice - discountPrice).toLocaleString()}원`}
+          primary
+        />
+        <PaymentSummaryItem
+          title="인앱 결제는 차후 추가될 예정입니다!"
+          price=" "
           primary
         />
       </S.PaymentSummaryItemList>

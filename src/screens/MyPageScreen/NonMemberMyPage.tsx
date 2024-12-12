c5341a8faa527109d6aa2e7def0c6b5e9bc1a87b
99mini <itman390@gmail.com>
2024-12-12 17:43:13 +0900

    feat: implement NonMemberMyPage component with styled elements and improved layout

--- src/screens/MyPageScreen/NonMemberMyPage.tsx
+++ src/screens/MyPageScreen/NonMemberMyPage.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
-import {View} from 'react-native';
-import {Button} from 'react-native-paper';
+
+import S from './NonMemberMyPage.style';
 
 const NonMemberMyPage = ({onPress}: {onPress: () => void}) => {
   return (
-    <View>
-      <Button onPress={onPress}>Go to Login</Button>
-    </View>
+    <S.Container>
+      <S.Description>
+        {'로그인 후 판매 중인 반찬을 예약해보세요.'}
+      </S.Description>
+      <S.LoginButton onPress={onPress}>
+        {'로그인 및 회원가입하기'}
+      </S.LoginButton>
+    </S.Container>
   );
 };
 

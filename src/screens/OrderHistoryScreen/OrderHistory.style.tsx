e258a90ce2477c7a18ed1bcdbf38c570737b4183
l-lyun <kimdohyun032@gmail.com>
2025-03-07 16:18:33 +0900

    style: 주문 내역 페이지 배경색 변경

--- src/screens/OrderHistoryScreen/OrderHistory.style.tsx
+++ src/screens/OrderHistoryScreen/OrderHistory.style.tsx
@@ -1,6 +1,9 @@
 import styled from '@emotion/native';
 
-const OrderHistoryContainer = styled.ScrollView``;
+const OrderHistoryContainer = styled.ScrollView`
+  flex: 1;
+  background-color: white;
+`;
 
 const S = {
   OrderHistoryContainer,

063eb3d3bbc52890e2bdd51fe26554f70d360d20
l-lyun <kimdohyun032@gmail.com>
2025-03-08 20:51:10 +0900

    feat: 별점 컴포넌트 작성

---
+++ src/components/review/RatingStarts.tsx
@@ -0,0 +1,27 @@
+import React, {useState} from 'react';
+import {View, StyleSheet, TouchableOpacity} from 'react-native';
+import S from './RaitingStarts.style';
+import Icon from 'react-native-vector-icons/AntDesign';
+
+type RatingStarsProps = {
+  rating: number;
+  setRating: (rating: number) => void;
+};
+
+const RatingStars = ({rating, setRating}: RatingStarsProps) => {
+  return (
+    <S.Container>
+      {[1, 2, 3, 4, 5].map(value => (
+        <TouchableOpacity key={value} onPress={() => setRating(value)}>
+          <Icon
+            name={value <= rating ? 'star' : 'staro'}
+            size={42}
+            color="#FFD700"
+          />
+        </TouchableOpacity>
+      ))}
+    </S.Container>
+  );
+};
+
+export default RatingStars;

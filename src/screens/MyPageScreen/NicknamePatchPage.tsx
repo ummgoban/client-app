bd40b36bb295181f726d401ad4cf5301ed459045
l-lyun <kimdohyun032@gmail.com>
2025-03-17 12:57:54 +0900

    feat: 닉네임 변경 기능 추가

--- src/screens/MyPageScreen/NicknamePatchPage.tsx
+++ src/screens/MyPageScreen/NicknamePatchPage.tsx
@@ -1,16 +1,55 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import S from './NoticePage.style';
+import useProfile from '@/hooks/useProfile';
+import TextInput from '@/components/common/TextInput/TextInput';
+import {usePatchNicknameMutation} from '@/apis/auth';
+import {Alert} from 'react-native';
+import {BottomButton} from '@/components/common';
+import {StackScreenProps} from '@react-navigation/stack';
+import {MyPageStackParamList} from '@/types/StackNavigationType';
+import {useQueryClient} from '@tanstack/react-query';
 
-const NicknamePatchPage = () => {
+type NicknamePatchScreenProps = StackScreenProps<
+  MyPageStackParamList,
+  'Nickname'
+>;
+
+const NicknamePatchPage = ({navigation}: NicknamePatchScreenProps) => {
+  const {profile} = useProfile();
+  const queryClient = useQueryClient();
+  const [nickname, setNickname] = useState<string>('');
+  const {mutate: nicknamePatchMutate} = usePatchNicknameMutation();
+
+  const handleNicknamePatchMutate = (nickname: string) => {
+    nicknamePatchMutate(nickname, {
+      onSuccess: () => {
+        Alert.alert('닉네임이 변경되었어요!');
+        navigation.goBack();
+        queryClient.invalidateQueries({queryKey: ['profile']});
+      },
+      onError: () => {
+        // FIXME: 에러 핸들링
+        Alert.alert('닉네인 변경에 실패했어요. 다시 시도해주세요!');
+      },
+    });
+  };
+
   return (
     <S.Container>
-      <S.NoticeCard>
-        <S.NoticeCardContent>
-          <S.Title>ㄴ</S.Title>
-          <S.Description>ㄹ</S.Description>
-        </S.NoticeCardContent>
-      </S.NoticeCard>
+      <TextInput
+        label="변경할 닉네임을 입력해주세요!"
+        placeholder={profile?.nickname ?? ''}
+        value={nickname}
+        onChange={e => setNickname(e.nativeEvent.text)}
+      />
+      <BottomButton
+        disabled={!nickname}
+        onPress={() => {
+          handleNicknamePatchMutate(nickname);
+        }}>
+        닉네임 변경하기
+      </BottomButton>
     </S.Container>
   );
 };
--- src/screens/MyPageScreen/NoticePage.style.tsx
+++ src/screens/MyPageScreen/NoticePage.style.tsx
@@ -1,38 +1,11 @@
 import styled from '@emotion/native';
 
-import {Card, Text} from 'react-native-paper';
-
-const Container = styled.ScrollView`
-  padding: 8px;
-  background-color: white;
-`;
-
-const NoticeCard = styled(Card)`
-  background-color: white;
-  padding: 12px 8px;
-`;
-
-const NoticeCardContent = styled.View`
-  display: flex;
-  flex-direction: column;
-
-  gap: 8px;
-`;
-
-const Title = styled(Text)`
-  ${({theme}) => theme.fonts.subtitle1};
-`;
-
-const Description = styled(Text)`
-  ${({theme}) => theme.fonts.body2};
-`;
-
 const S = {
-  Container,
-  NoticeCard,
-  NoticeCardContent,
-  Title,
-  Description,
+  Container: styled.View`
+    padding: 8px;
+    background-color: white;
+    flex: 1;
+  `,
 };
 
 export default S;

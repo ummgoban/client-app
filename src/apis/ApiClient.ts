bfbe9a9e573d49fbcf3ad86509b453b4fcab8b3e
99mini <itman390@gmail.com>
2025-04-30 19:36:17 +0900

    fix: enhance OAuth token handling and force logout on expiration

--- src/apis/ApiClient.ts
+++ src/apis/ApiClient.ts
@@ -24,25 +24,50 @@ class ApiClient {
     const session: SessionType | null = await getStorage('session');
     this._jwt = session?.accessToken ?? null;
 
-    if (!this._jwt) return;
+    if (!this._jwt) {
+      return;
+    }
 
-    const isAccessTokenExpired =
-      session?.accessTokenExpiresAt &&
-      session.accessTokenExpiresAt > Date.now();
+    /**
+     * OAuth 서비스에 따라 refreshToken이 없는 경우가 있어. AccessToken 만료 시 강제 로그아웃 처리
+     */
+    const isOAuthTokenExpired =
+      session &&
+      session.OAuthProvider !== 'BASIC' &&
+      session.accessTokenExpiresAt &&
+      session.accessTokenExpiresAt < Date.now();
 
-    const isValidRefreshToken =
-      session?.refreshToken &&
-      session.refreshTokenExpiresAt &&
-      session.refreshTokenExpiresAt > Date.now();
+    // Oauth 토큰이 만료된 경우에 강제 로그아웃
+    if (isOAuthTokenExpired) {
+      console.debug('OAuth token expired. Force Sign Out');
+      // setStorage('session', {});
+      this._jwt = null;
+      config.headers.Authorization = null;
+      return;
+    }
 
-    if (isAccessTokenExpired && isValidRefreshToken && session.refreshToken) {
-      const newSession = await refreshAccessToken(session.refreshToken);
-      if (newSession) {
-        config.headers.Authorization = `Bearer ${newSession.accessToken}`;
-        return;
+    // 리프레쉬 토큰이 있는 경우 갱신
+    // 리프레쉬 토큰 갱신 실패 시 강제 로그아웃
+    if (session?.refreshToken) {
+      try {
+        const newSession = await refreshAccessToken(session.refreshToken);
+
+        if (newSession) {
+          setStorage('session', newSession);
+
+          config.headers.Authorization = `Bearer ${newSession.accessToken}`;
+          return;
+        } else {
+          setStorage('session', {});
+          this._jwt = null;
+          config.headers.Authorization = null;
+        }
+      } catch (error) {
+        console.error('Error refreshing access token:', error);
+        setStorage('session', {});
+        this._jwt = null;
+        config.headers.Authorization = null;
       }
-    } else {
-      await setStorage('session', {});
     }
 
     config.headers.Authorization = `Bearer ${this._jwt}`;

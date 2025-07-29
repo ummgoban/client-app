e1113518d3a182e100c891b808a4935501f4324a
99mini <itman390@gmail.com>
2025-07-29 12:46:46 +0900

    test: Jest 설정 및 네이티브 모듈 모킹 추가

--- jest.config.js
+++ jest.config.js
@@ -7,4 +7,10 @@ module.exports = {
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   testRegex: '(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$',
   collectCoverage: true,
+  // ESM 모듈을 사용하는 패키지를 변환할 수 있도록 설정
+  transformIgnorePatterns: [
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|react-navigation|@react-navigation/.*|react-native-splash-screen|@?react-native-community/.*|@react-native/.*|react-native-vector-icons|react-native-async-storage)',
+  ],
+  // SplashScreen과 같은 네이티브 모듈을 모킹
+  setupFiles: ['./jest.setup.js'],
 };

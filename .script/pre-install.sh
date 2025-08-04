6f6d788313f2231b5f3b1822c80036b2c3fb1f68
99mini <itman390@gmail.com>
2025-08-04 16:36:15 +0900

    chore: remove unnessary script

--- .script/pre-install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "=== preinstall ==="
-
-# 원본 유지
-cp .yarnrc.yml .yarnrc.yml.temp
-
-NPM_TOKEN=$(grep NPM_TOKEN .env | cut -d '=' -f2 | tr -d '"' | xargs) envsubst < .yarnrc.yml.template > .yarnrc.yml

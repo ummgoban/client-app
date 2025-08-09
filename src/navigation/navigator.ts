829bbe9a41e84dbb30521b3cea60d383a5365d17
99mini <itman390@gmail.com>
2025-08-09 22:24:59 +0900

    fix: 개발환경에서 사용

--- src/navigation/navigator.ts
+++ src/navigation/navigator.ts
@@ -23,7 +23,9 @@ export function routeToDetail(
     screen: 'MarketDetail',
     params: {marketId},
     webview: {
-      uri: __DEV__ ? 'http://172.30.1.34:4173' : 'https://ummgoban.com',
+      uri: __DEV__
+        ? `http://192.168.66.12:5173/market/${marketId}`
+        : 'https://ummgoban.com',
       title: 'webview',
     },
   });

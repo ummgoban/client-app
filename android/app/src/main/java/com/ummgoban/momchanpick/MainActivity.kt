aa7ad084adb1176dfc7bfaf3f5649e53bc44c5c1
99mini <itman390@gmail.com>
2025-09-04 21:18:52 +0900

    chore: 안드로이드 사진 권한 수정

--- android/app/src/main/java/com/ummgoban/momchanpick/MainActivity.kt
+++ android/app/src/main/java/com/ummgoban/momchanpick/MainActivity.kt
@@ -8,7 +8,7 @@ import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
-import org.devio.rn.splashscreen.SplashScreen;
+import org.devio.rn.splashscreen.SplashScreen
 
 class MainActivity : ReactActivity() {
 
